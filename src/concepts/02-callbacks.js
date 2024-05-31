be25ecf1c0686ddf57d03cb99756251d76aef93b
Gabi13st.h <gabs13.h@gmail.com>
2024-05-30 22:50:53 -0400

    Agrega validación para errores en callback

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -8,25 +8,35 @@ import { heroes } from '../data/heroes';
 
 export const callbacksComponent = (element) => {
 
-    const id = '5d86371f2343e37870b91ef1';
-    findHero(id, (hero) => {
+    const id = '5d86371f2343e37870b91ef1s';
+    findHero(id, (error, hero) => {
+        // element.innerHTML = hero?.name || 'No hero found';
+
+        if (error) {
+            element.innerHTML = error;  
+            return;
+        }
+
         element.innerHTML = hero.name;
     })
 
     
 }
 
-
 /**
  * 
  * @param {String} id 
- * @param { (hero: Object)=> void } callback 
+ * @param { (error: String|Null, hero: Object)=> void } callback 
  */
 const findHero = (id, callback) => {
 
     const hero = heroes.find( hero => hero.id === id );
 
-    callback(hero);
+    if (!hero) {
+        callback( `Hero with ${id} not found`);
+        return;
+    }
 
+    callback(null, hero); 
 
 }
