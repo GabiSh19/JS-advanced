b297f265ee088b1b87761a22c04a819c26d878f7
Gabi13st.h <gabs13.h@gmail.com>
2024-06-01 22:55:31 -0400

    agrega lógica de promise Hell

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -11,6 +11,10 @@ export const promiseComponent = (element) => {
         element.innerHTML = `Nombre del héroe: ${hero.name}`;
     }
 
+    const renderTwoHeroes = (hero1, hero2) => {
+        element.innerHTML = `Nombre del primer heroe: ${hero1.name} <br/> Nombre del segundo heroe: ${hero2.name}`
+    }
+
     const renderError = (error) => {
         element.innerHTML = `
         <h2> Error:</h2>
@@ -20,12 +24,21 @@ export const promiseComponent = (element) => {
     
 
     const id1 = '5d86371f2343e37870b91ef1';
+    const id2 = '5d86371f25a058e5b1c8a65e';
 
     findHero(id1)
         .then( superHero => renderHero(superHero) ) //En este caso podríamos simplicarlo de la siguiente manera: 
         // .then( renderHero ) > Siempre y cuando los argumentos sean los mismos, en el mismo órden.
         .catch(renderError);
 
+    //Con PROMISE HELL, sería lo siguiente: 
+    // findHero(id1)
+    // .then((hero1) => {
+    //     findHero(id2)
+    //     .then(hero2 => renderTwoHeroes(hero1, hero2))
+    //     .catch(renderError);
+    // })
+    // .catch(renderError);
 }
 
 /**
