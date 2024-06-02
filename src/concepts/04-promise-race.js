d6334469386a4a5e4added1a207bf26199d72f2c
Gabi13st.h <gabs13.h@gmail.com>
2024-06-02 00:05:59 -0400

    Crea 3 promesas en promise Race

--- src/concepts/04-promise-race.js
+++ src/concepts/04-promise-race.js
@@ -8,7 +8,29 @@ export const promiseRaceComponent = (element) => {
     const renderValue = ( value ) => {
         element.innerHTML = value;
     }
+
+    Promise.race ([
+        slowPromise(),
+        mediumPromise(),
+        fastPromise(),
+    ]).then( value => renderValue( value ));
     
 }
 
-const slowPromise 
+const slowPromise = () => new Promise ( resolve => {
+    setTimeout( () => {
+        resolve('Slow Promise');
+    }, 2000  )
+});
+
+const mediumPromise = () => new Promise ( resolve => {
+    setTimeout( () => {
+        resolve('Medium Promise');
+    }, 1500  )
+});
+
+const fastPromise = () => new Promise ( resolve => {
+    setTimeout( () => {
+        resolve('Fast Promise');
+    }, 1000  )
+});
