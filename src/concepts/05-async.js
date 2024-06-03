baccc988aa752e557f396fac03057e81674ed3fd
Gabi13st.h <gabs13.h@gmail.com>
2024-06-02 20:45:23 -0400

    Complementa código async

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -1,11 +1,33 @@
+import { heroes } from "../data/heroes";
+
 /**
  * 
  * @param {HTMLDivElement} element 
  */
 
+
 export const asyncComponent = (element) => {
 
-    console.log('asyncComponent')
+    const id1 = '5d86371f25a058e5b1c8a65e';
 
-    
+    findHeroes(id1)
+    .then(name => element.innerHTML = name)
+    .catch(error => element.innerHTML = error);
+
 }
+
+
+/**
+ * 
+ * @param {String} id 
+ * @returns {Promise<String>}
+ */
+const findHeroes = async( id ) => {
+
+    const hero = heroes.find( hero => hero.id === id ); 
+    if (!hero)
+        throw  `Hero with id ${id} not found`;
+
+    return hero.name;
+
+} 
