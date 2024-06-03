eaeb325e4e805903dfb742e7163c5db2fd761f96
Gabi13st.h <gabs13.h@gmail.com>
2024-06-02 23:54:35 -0400

    Agrega lógica en async await

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -1,11 +1,23 @@
+import { heroes } from "../data/heroes";
+
 /**
  * 
  * @param {HTMLDivElement} element 
  */
 
+
 export const asyncAwaitComponent = (element) => {
 
-    console.log('asyncAwaitComponent');
+    const id1 = '5d86371f97c29d020f1e1f6d';
+    const id2 = '5d86371fd55e2e2a30fe1ccb2';
+
+    findHero(id1, id2)
+    .then()
+    
 
 
+}
+
+const findHero = async (id) => {
+    const hero = heroes.find(hero => hero.id === id);
 }
