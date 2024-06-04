f404e0566001a969523e4d13eacf9ead043cd602
Gabi13st.h <gabs13.h@gmail.com>
2024-06-03 21:51:25 -0400

    actualiza archivo 07

--- src/concepts/07-async-await.js
+++ src/concepts/07-async-await.js
@@ -3,9 +3,40 @@
  * @param {HTMLDivElement} element 
  */
 
-export const demoComponent = (element) => {
+export const asyncAwait2Component = async(element) => {
 
-
-
+    console.time('Start');
     
+    const value1= await slowPromise();
+    const value2= await mediumPromise();
+    const value3= await fastPromise();
+    
+    element.innerHTML = `
+
+        value1: ${value1}
+        value2: ${value1}
+        value3: ${value1}
+    
+    `
+
+    console.timeEnd('Start');
+
 }
+
+const slowPromise = () => new Promise ( resolve => {
+    setTimeout( () => {
+        resolve('Slow Promise');
+    }, 2000  )
+});
+
+const mediumPromise = () => new Promise ( resolve => {
+    setTimeout( () => {
+        resolve('Medium Promise');
+    }, 1500  )
+});
+
+const fastPromise = () => new Promise ( resolve => {
+    setTimeout( () => {
+        resolve('Fast Promise');
+    }, 1000  )
+});
