d152cfcc3286d444f69c5ded3026e46a714f3df2
Gabi13st.h <gabs13.h@gmail.com>
2024-06-03 21:37:37 -0400

    Agrega nuevo ejercicio de async await

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -11,7 +11,7 @@ export const asyncAwaitComponent = async(element) => {
     const id1 = '5d86371f97c29d020f1e1f6d';
     const id2 = '5d86371fd55e2e2a30fe1ccb2';
 
-    //! Async await optimizado (Para promesas que no dependan la una de la otra):
+    //! Async await optimizado (Para promesas que no dependan la una de la otra = NO SECUENCIALES):
 
     
 
@@ -21,7 +21,7 @@ export const asyncAwaitComponent = async(element) => {
     // try {
     //     const hero1 = await findHero(id1);
     //     const hero2 = await findHero(id2);
-
+ 
     //     element.innerHTML = `${hero1.name} / ${hero2.name}`
 
     // } catch (error) {
